0d9438997720a7fe3e40152ff971995771ef6d98
maktimus <72384311+maktimus@users.noreply.github.com>
2024-01-20 14:54:25 +0000

    still to complete movement

--- pkm/Assets/PlayerMovement.cs
+++ pkm/Assets/PlayerMovement.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
+[RequireComponent(typeof(PlayerInput))]
 public class PlayerMovement : MonoBehaviour
 {
     public float runSpeed;
@@ -10,22 +11,31 @@ public class PlayerMovement : MonoBehaviour
     //will need to be an array or list when implementing a party of pets. Same for other revelant scripts
     public GameObject pet;
 
+    private PlayerInput input;
+    private InputAction m_Move;
+    private Rigidbody rb;
+  
     // Start is called before the first frame update
     void Start()
     {
+     
         followScript = pet.GetComponent<Follow>();
+        rb = GetComponent<Rigidbody>();
+      
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(followScript.battle == true && this.tag == "Player")
+        if(followScript.battle == true)
         {
-            runSpeed = 0;
+            //PlayerInput input = new PlayerInput();
+            //input.DeactivateInput();
+            Debug.Log("deactivated");
         }
         else
         {
-            runSpeed = 0.05f;
+            //runSpeed = 0.05f;
         }
         /*
         if (followScript.battle == true && this.tag == "Pet")
@@ -37,11 +47,29 @@ public class PlayerMovement : MonoBehaviour
             runSpeed = 0.05f;
         }*/
 
-        Vector3 horizontal = new Vector3(Input.GetAxis("Horizontal"), 0.0f, 0.0f);
+        /*Vector3 horizontal = new Vector3(Input.GetAxis("Horizontal"), 0.0f, 0.0f);
         transform.position = transform.position + horizontal * runSpeed;
 
         Vector3 vertical = new Vector3( 0.0f, 0.0f, Input.GetAxis("Vertical"));
-        transform.position = transform.position + vertical * runSpeed;
+        transform.position = transform.position + vertical * runSpeed;*/
+
+        if(input == null)
+        {
+            input = GetComponent<PlayerInput>();
+            m_Move = input.actions["move"];
+        }
+
+        var move = m_Move.ReadValue<Vector3>();
+
+
     }
+
+
+    public void OnMove(InputValue value)
+    {
+        rb.velocity = value.Get<Vector3>() * runSpeed;
+    }
+
+
 
 }
